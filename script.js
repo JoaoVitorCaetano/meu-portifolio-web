41f2cd1e59bb0c3707df653db23ee338bd618d8d
JoaoVitorCaetano <joaovitorcaetano2808@gmail.com>
2026-03-06 18:06:08 -0300

    adicionando funcionalidade nos inputs de turno para adicionar telefone

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const selectSerie = document.getElementById('serie');
 const blocoLingua = document.getElementById('bloco-segunda-lingua');
 const radiosLingua = document.querySelectorAll('input[name="segunda-lingua"]');
 
+const blocoTelefone = document.getElementById('bloco-telefone-tarde');
+const radiosTurno = document.querySelectorAll('input[name="turno"]');
+
 
 
 selectSerie.addEventListener('change', function(){
@@ -13,3 +16,14 @@ selectSerie.addEventListener('change', function(){
         radiosLingua.forEach(r => r.checked = false)
     }
 });
+
+radiosTurno.forEach(radio => {
+    radio.addEventListener('change', function(){
+        if (this.value === 'tarde'){
+            blocoTelefone.style.display = 'block';
+        }
+        else {
+            blocoTelefone.style.display = 'none'
+        }
+    })
+})
